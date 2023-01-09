266b03de13cee545c67a303a59b2491e5e61f49d
Thomas AGUIRREGABIRIA <hello@itag-technology.com>
2023-01-09 23:34:27 +0100

    home page tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,13 @@
-import { render } from "@testing-library/react"
-import { describe, expect, it } from "vitest"
+import React from "react"
+import { render, screen, waitFor } from "@testing-library/react"
+import { rest } from "msw"
+import { setupServer } from 'msw/node'
+import { afterAll, beforeAll, describe, expect, it, vi } from "vitest"
+import Home from "../pages/index"
 import HomePage from "../pages/index"
 import App from "../pages/_app"
 
-describe("Home page", () => {
+describe("Home page add a usal snapshot", () => {
   it("should render as usual", () => {
     // when ----------------
     // @ts-ignore - FIXME: fix this typescript error
@@ -11,5 +15,72 @@ describe("Home page", () => {
 
     // then ----------------
     expect(container).toMatchSnapshot()
+  })
+})
+
+describe("Home render a movie collection", () => {
+  const server = setupServer(
+    rest.get("http://www.omdbapi.com/?apikey=4a3b711b&s=Batman", (_, res, ctx) => {
+      return res(
+        ctx.json({
+          Search: [
+            {
+              Title: "Batman Begins",
+              Year: "2005",
+              imdbID: "tt0372784",
+              Type: "movie",
+              Poster: "https://m.media-amazon.com/images/M/MV5BMTMwNjQ5NjQxN15BMl5BanBnXkFtZTgwNjQ5NjQxMzE@._V1_SX300.jpg",
+            },
+            {
+              Title: "Batman v Superman: Dawn of Justice",
+              Year: "2016",
+              imdbID: "tt2975590",
+              Type: "movie",
+              Poster: "https://m.media-amazon.com/images/M/MV5BMTMwNjQ5NjQxN15BMl5BanBnXkFtZTgwNjQ5NjQxMzE@._V1_SX300.jpg",
+            },
+            {
+              Title: "Batman",
+              Year: "1989",
+              imdbID: "tt0096895",
+              Type: "movie",
+              Poster: "https://m.media-amazon.com/images/M/MV5BMTMwNjQ5NjQxN15BMl5BanBnXkFtZTgwNjQ5NjQxMzE@._V1_SX300.jpg",
+            },
+          ],
+          totalResults: "3",
+          Response: "True",
+        })
+      )
+    })
+  )
+
+  beforeAll(async () => {
+    // start the server
+    server.listen()
+
+    // given -------------------
+    // mock the react context to simulate a user search
+    const fakeSearch = { search: 'Batman' }
+    vi.spyOn(React, "useContext").mockImplementation(() => fakeSearch)
+
+    // render the component
+    render(<Home />)
+
+    // wait for the data to be fetched
+    await waitFor(() => {
+      expect(screen.findByText("Batman Begins"))
+    })
+
+  })
+
+  afterAll(() => {
+    server.close()
+  })
+
+  it("should display 3 movie cards", async () => {
+    // when -------------------
+    const movieCardElements = await screen.findAllByTestId("movie-link")
+
+    // then -------------------
+    expect(movieCardElements).toHaveLength(3)
   })
 })
